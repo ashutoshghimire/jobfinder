93113073d9ded37761b5f7975115445290402269
ashutoshghimire <ashutoshlove2060@gmail.com>
2025-01-27 11:58:46 +0545

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,18 @@
+import ashutosh from "./ashutosh.png";
+import Facebook from "./facebook.png";
+import Google from "./google.png";
+import HeroImage from "./hero.png";
+import Instagram from "./instagram.png";
+import JobImg from "./job.jpg";
+import Linkedin from "./linkedin.png";
+import Office from "./office.jpg";
+import Spotify from "./spotify.png";
+import Twitter from "./twitter.png";
+import NoProfile from "./userprofile.png";
+import WhatsApp from "./whatsapp.png";
+import Youtube from "./youtube.png";
+
+export {
+  ashutosh, Facebook, Google, HeroImage, Instagram, JobImg, Linkedin, NoProfile, Office, Spotify, Twitter, WhatsApp, Youtube
+};
+
